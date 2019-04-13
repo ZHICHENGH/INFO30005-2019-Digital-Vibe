99edafa56908c524812c95c8e94659806c429154
ZHICHENGH <HZC@users.noreply.github.com>
2019-04-14 01:09:16 +1000

    Update controller_user.js

--- controllers/controller_user.js
+++ controllers/controller_user.js
@@ -48,11 +48,7 @@ var finduserByName = function(req, res){
 var deleteuserById =function(req, res){
     var userInx = req.params.id;
     User.findOneAndDelete({user_id:userInx}, function(req,res){
-        if(!err){
             res.send("delete success!");
-        }
-        else
-        res.send("fail to delete");
     });
 };
 
