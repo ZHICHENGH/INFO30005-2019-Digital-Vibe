23a446a95383748aa218fb62947fda1356ab7183
ZHICHENGH <HZC@users.noreply.github.com>
2019-04-14 00:20:02 +1000

    Update route_comment.js

--- routes/route_comment.js
+++ routes/route_comment.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/controller_comment.js');
+var express = require('express');
+var router = express.Router();
+var controller = require('../controllers/controller_comment.js');
 
 
 
 // Get all places
-places_router.post('/commentAPI', controller.createComment);
+router.post('/commentAPI', controller.createComment);
 
 // Get a specific place by name
-places_router.get('/comentAPI/:place_id', controller.getcommentsbyplaceid);
+router.get('/comentAPI/:place_id', controller.getcommentsbyplaceid);
 
 // Create a new place
-places_router.delete('/placeAPI/:place_id/:user_id/:comment_id', controller.deletecomment);
+router.delete('/placeAPI/:place_id/:user_id/:comment_id', controller.deletecomment);
 
 // update a place
-places_router.get('/placeAPI/:user_id', controller.getcommentsbyuserid);
+router.get('/placeAPI/:user_id', controller.getcommentsbyuserid);
 
 
 module.exports = router;
