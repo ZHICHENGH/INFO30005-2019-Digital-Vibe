211d6b5b4453f98e49860aff7aef5a7ecd17fb00
ZHICHENGH <HZC@users.noreply.github.com>
2019-04-13 14:46:18 +1000

    Create route_user.js

---
+++ routes/route_user.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+var controller = require('../controllers/controller_user.js');
+
+// Create new verifyPlace
+router.post('/api',controller.createuser);
+//get all users
+router.get('/api',controller.findAllusers);
+//get user by id
+router.get('/api/id/:id',controller.findOneuser);
+//get user by name
+router.get('/api/name/:name',controller.finduserByName);
+//delete user by ID
+router.get('/api/id/:id',controller.deleteuserById);
