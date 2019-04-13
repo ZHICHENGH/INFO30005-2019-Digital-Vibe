cba8c2f686c629dde652ab83abeeb3822cd6c1d7
Joean Yun <joeany@student.unimelb.edu.au>
2019-04-13 22:06:30 +1000

    Adding the rest of verifyPlace APIs

--- controllers/controller_verifyPlace.js
+++ controllers/controller_verifyPlace.js
@@ -17,4 +17,67 @@ var createVerifyPlace = function(req,res){
     });
 };
 
+var findOneVerifyPlace = function(req,res){
+    var req_place_id = req.params.place_id;
+    var req_user_id = req.params.user_id;
+    var req_acc_type = req.params.acc_type;
+
+    VerifyPlace.find({
+            place_id:req_place_id,
+            user_id:req_user_id,
+            acc_type:req_acc_type
+    },function(err,verifyPlace){
+        if(!err){
+            res.send(verifyPlace);
+        }else{
+            res.sendStatus(404);
+        }
+    });
+};
+
+var findVerifyPlaceByPlaceAccType = function(req,res){
+    var req_place_id = req.params.place_id;
+    var req_acc_type = req.params.acc_type;
+
+    VerifyPlace.find({
+        place_id:req_place_id,
+        acc_type:req_acc_type
+    },function(err,verifyPlace){
+        if(!err){
+            res.send(verifyPlace);
+        }else{
+            res.sendStatus(404);
+        }
+    });
+};
+
+var findAllVerifyPlace = function(req,res){
+    VerifyPlace.find({},function(err,verifyPlace){
+        if(!err){
+            res.send(verifyPlace);
+        }else{
+            res.sendStatus(404);
+        }
+    });
+};
+
+var deleteOneVerifyPlace = function(req,res){
+    var req_place_id = req.params.place_id;
+    var req_user_id = req.params.user_id;
+    var req_acc_type = req.params.acc_type;
+
+    VerifyPlace.deleteOne({
+        place_id:req_place_id,
+        user_id:req_user_id,
+        acc_type:req_acc_type},function(err){
+        if(err){
+            res.sendStatus(404);
+        }
+    });
+};
+
 module.exports.createVerifyPlace = createVerifyPlace;
+module.exports.findOneVerifyPlace = findOneVerifyPlace;
+module.exports.findVerifyPlaceByPlaceAccType = findVerifyPlaceByPlaceAccType;
+module.exports.findAllVerifyPlace = findAllVerifyPlace;
+module.exports.deleteOneVerifyPlace = deleteOneVerifyPlace;
--- routes/route_verifyPlace.js
+++ routes/route_verifyPlace.js
@@ -6,4 +6,16 @@ var controller = require('../controllers/controller_verifyPlace.js');
 // Create new verifyPlace
 router.post('/verifyPlaceAPI',controller.createVerifyPlace);
 
+// Find all verifyPlace
+router.get('/verifyPlaceAPI',controller.findAllVerifyPlace);
+
+// Find one verifyPlace
+router.get('/verifyPlaceAPI/:place_id/:user_id/:acc_type',controller.findOneVerifyPlace);
+
+// Find verifyPlaces by Place and Acc_Type
+router.get('/verifyPlaceAPI/:place_id/:acc_type',controller.findAllVerifyPlace);
+
+// Delete one verifyPlace
+router.delete('/verifyPlaceAPI/:place_id/:user_id/:acc_type',controller.deleteOneVerifyPlace);
+
 module.exports = router;
