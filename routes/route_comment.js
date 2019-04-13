3e4a1d7fd543043a415ea2e9c50cf04041bbd34e
dongchengd1 <48665115+rexding97@users.noreply.github.com>
2019-04-14 00:04:07 +1000

    Update route_comment.js

--- routes/route_comment.js
+++ routes/route_comment.js
@@ -10,10 +10,10 @@ places_router.get('/commentAPI', controller.createComment);
 places_router.get('/comentAPI/:place_id', controller.getcommentsbyplaceid);
 
 // Create a new place
-places_router.post('/placeAPI/:place_id/:user_id/:comment_id', controller.deletecomment);
+places_router.delete('/placeAPI/:place_id/:user_id/:comment_id', controller.deletecomment);
 
 // update a place
-places_router.put('/placeAPI/:user_id', controller.getcommentsbyuserid);
+places_router.get('/placeAPI/:user_id', controller.getcommentsbyuserid);
 
 
 module.exports = comment_router;
