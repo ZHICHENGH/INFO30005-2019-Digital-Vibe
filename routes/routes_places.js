74736b4eb8d803e9732b0efff8da23ffd36f1cc2
Redatee <48498844+Redatee@users.noreply.github.com>
2019-04-13 23:50:20 +1000

    Update routes_places.js

--- routes/routes_places.js
+++ routes/routes_places.js
@@ -13,7 +13,7 @@ places_router.get('/placeAPI/name/:name', controller.getPlaceByName);
 places_router.post('/placeAPI', controller.createPlace);
 
 // update a place
-places_router.put('/placeAPI', controller.updatePlaceById);
+places_router.put('/placeAPI/id/:id', controller.updatePlaceById);
 
 
 module.exports = places_router;
