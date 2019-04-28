918773fb34151be501897288a0e46ee895eb5e3f
Joean Yun <joeany@student.unimelb.edu.au>
2019-04-28 14:49:17 +1000

    Further testing app.js

--- app.js
+++ app.js
@@ -29,8 +29,12 @@ app.use('/comment', comment_route);
 // HTML files
 app.get('/', function(request, response){
     response.sendfile('index.html');
+});
+
+app.get('/GetAccPlaces', function(request, response){
     response.sendfile('GetAccPlaces.html');
 });
+
 
 // Start the server
 const PORT = process.env.PORT || 3000;
