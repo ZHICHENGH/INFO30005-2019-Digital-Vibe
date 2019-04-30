f411c09c9d591b45a39930265a12dfd8b67c6d40
Joean Yun <joeany@student.unimelb.edu.au>
2019-04-30 21:43:19 +1000

    Re-enter CORS codes post merging

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Enable CORS in order to facilitate localhost testing
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Database setup
 require('./models/db.js');
