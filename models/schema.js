70a2cd495e88b1e05d3b8d12423b82a3c2b06913
Redatee <48498844+Redatee@users.noreply.github.com>
2019-05-07 18:49:24 +1000

    Update schema.js

--- models/schema.js
+++ models/schema.js
@@ -41,6 +41,6 @@ var verifyPlaceSchema = mongoose.Schema(
 );
 
 mongoose.model('place',placeSchema);
-module.exports.mongoose.model('user',userSchema);
+mongoose.model('user',userSchema);
 mongoose.model('comment',commentSchema);
 mongoose.model('verifyPlace',verifyPlaceSchema);
